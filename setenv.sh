b4b8306363623677a9706018b73b9fd169070d2d
Alex Lundgren <alex@endem.su>
2023-05-10 01:33:48 +0800

    ENVS

--- setenv.sh
+++ setenv.sh
@@ -1,7 +1,3 @@
-#!/bin/sh
-
-# Активировать окружение
-
-export PYTHONPATH=$PYTHONPATH:$(pwd)
-. .venv/bin/activate
-
+version https://git-lfs.github.com/spec/v1
+oid sha256:5a3738258700c4557427b167d8b3e9e80f87e092d89ac0b80ce491dbd3b5b8e8
+size 70
